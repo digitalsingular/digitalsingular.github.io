3cacc0680bfdb3d90650fe167c487dec374bd3a6
Agustin Ventura <agustinventura@gmail.com>
2016-06-02 12:44:34 +0200

    Added publishing script

---
+++ bakeAndPublish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+jbake -b
+git add .
+git commit
+git push origin master
+cp -R output/* ../../../agustinventura.github.io/
+cd ../../../agustinventura.github.io/
+git add .
+git commit
+git push origin master
