de99032c5609149d43e2d7c86df7a7659fe0e034
Agustin Ventura <agustinventura@gmail.com>
2016-06-06 13:22:56 +0200

    Modified publishing script to delete output, so there's no trash left after tests.

--- bakeAndPublish.sh
+++ bakeAndPublish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -rf output
 jbake -b
 git add .
 git commit
 git push origin master
+rm -rf ../agustinventura.github.io/*
 cp -R output/* ../agustinventura.github.io/
 cd ../agustinventura.github.io/
 git add .
