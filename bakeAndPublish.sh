c0233a85211f09edc2c7257eb430ebe2b780106d
Agustín Ventura <agustinventura@gmail.com>
2018-12-28 13:08:53 +0100

    adds post on certificate and device configuration

--- bakeAndPublish.sh
+++ bakeAndPublish.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-rm -rf output
 jbake -b
 git add .
 git commit
 git push origin master
-rm -rf ../agustinventura.github.io/*
 cp -R output/* ../agustinventura.github.io/
 cd ../agustinventura.github.io/
 git add .
